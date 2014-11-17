af402cd73b10884d3212609dcc59c1e876c71801
aaextention <aaextention@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-11-17 02:31:11 +0000

    update
    git-svn-id: https://plugins.svn.wordpress.org/aa-block-country/trunk@1026966 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- aablockcountry.php
+++ aablockcountry.php
@@ -1,12 +1,12 @@
 <?php 
 /**
  * Plugin Name: AA Block Country
- * Plugin URI: http://wordpress.org/.../
- * Description: Go to setting
+ * Plugin URI: https://wordpress.org/plugins/aa-block-country/
+ * Description: Please use settings to add country to block visitors from there :)
  * Version: 1.0
  * Developer + Idea: A. Roy / A. Mahmud
  * Author URI: http://webdesigncr3ator.com
- * Support Email : contactus.aa@gmail.com
+ * Support Email : contact2us.aa@gmail.com
  * License: GPL2
  **/
 	
